f7f1a2b3cfff1ea18206ea8eb8ce456827bb18ff
juan <juanzan92@gmail.com>
2021-01-26 16:14:39 -0300

    Initial commit

---
+++ src/main/java/com/microservice/intercorp/IntercorpApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.intercorp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntercorpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntercorpApplication.class, args);
+	}
+
+}
